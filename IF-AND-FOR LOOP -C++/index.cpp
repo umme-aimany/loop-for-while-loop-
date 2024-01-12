9618db060e0ec5a3dc1cc62cb760e145a87c5ee7
Umme Aiman <122363026+umme-aimany@users.noreply.github.com>
2024-01-12 21:15:05 +0500

    Add files via upload

---
+++ IF-AND-FOR LOOP -C++/index.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+
+
+
+
+
+
+
+
+
+/////////// for looop//////////////////////////////
+
+int main()
+{
+int i=1;
+while(i<=500)
+{
+    std::cout<< i;
+    i=i+1;
+}
+    return 0;
+}
+/////////////table 5////////////////////////////
+
+int main()
+{
+int i=1;
+while(i<=20)
+{
+    std::cout<< "5*" <<i<<  "=" <<5*i<< "are" ;
+    i=i+1;
+}
+}
